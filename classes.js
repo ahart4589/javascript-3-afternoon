fbe1abe481e5299fbd625aebb1b74798d6ce27ed
Alexandria Hart <alexandriahart@moondog.local>
2018-07-11 14:13:02 -0600

    initial commit

--- classes.js
+++ classes.js
@@ -30,7 +30,19 @@
 */
 
 //Code Here
+class Employee {
+  constructor(first_name,last_name,email,age){
+    this.first_name = first_name;
+    this.last_name = last_name;
+    this.email = email;
+    this.age = age;
+  }
+  makeWidget(){
+    return this.first_name  +' '+this.last_name + ' Widget'
+  }
 
+}
+var newEmployee = new Employee('Alexa','Hart','a@gmailcom',29)
 
 
 ////////// PROBLEM 2 //////////
